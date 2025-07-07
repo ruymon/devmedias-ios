a56b5196e425e870ea52a48a76fbe5a5f218c793
ruymon <ruystfrancis@gmail.com>
2025-07-07 19:16:43 -0300

    Initial Commit

---
+++ devmedias-ios/devmedias_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  devmedias_iosApp.swift
+//  devmedias-ios
+//
+//  Created by Ruy Monteiro on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct devmedias_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ devmedias-iosTests/devmedias_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  devmedias_iosTests.swift
+//  devmedias-iosTests
+//
+//  Created by Ruy Monteiro on 07/07/25.
+//
+
+import Testing
+@testable import devmedias_ios
+
+struct devmedias_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
